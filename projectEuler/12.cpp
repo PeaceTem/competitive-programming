182c89df96bee8efa6990de84c8f752af4d95e79
PeaceTem <olumidejonathan10@gmail.com>
2025-02-28 11:04:42 +0100

    solved problem 12 even though I don't know how it works

--- projectEuler/12.cpp
+++ projectEuler/12.cpp
@@ -8,7 +8,7 @@ vector<bool> sieve(int n){
 
     for(int i=2;i*i<=n;i++){
         if(primes[i]){
-            for(int j=i*i;j<=n;j++) primes[j]=false;
+            for(int j=i*i;j<=n;j+=i) primes[j]=false;
         }
     }
     return primes;
@@ -22,37 +22,41 @@ int main(){
         if(primes[i]) primes_list.push_back(i);
     }
 
-    long long a = 2;
+    for(int i=0; i<primes_list.size(); i++) cout << primes_list[i] << ' ';
+    cout << '\n';
+    long long a = 2, nums, term, _term;
     bool done = false;
-    long long nums;
-    long long term,_term;
-    vector<int> nums_list,__nums_list;
+    vector<int> nums_list,__nums_list, factors;
     int value;
-    vector<int> factors;
+
     while(!done){
         a++; nums=0;
-        term = (a*(a+1LL))/2LL; 
-        _term = term;
-        for(int i=0;primes_list[i]*primes_list[i]<=term; i++){
-            factors.clear();
-            while(term%primes_list[i]==0){
-                nums++; term/=primes_list[i];
-                factors.push_back(primes_list[i]);
-            }
-            nums_list.push_back(nums++);
+        _term = term = (a*(a+1LL))/2LL; 
+        cout << a << ' ' << term << '\n';
+        factors.clear();
+        for(int i=0; i< primes_list.size() && primes_list[i]*primes_list[i]<=term; i++){
+            if(term <=1) break;
             nums=0;
+            while(term%primes_list[i]==0 && term > 1){
+                nums++; term/=primes_list[i];
+                factors.push_back(primes_list[i]); // use map here;
+            }
+            nums_list.push_back(++nums);
         }
-        value = accumulate(nums_list.begin(),nums_list.end(), 1, multiplies<int>());
-        done = value>4 ? true : false;
+        if(term >1){
+            nums_list.push_back(2);
+        }
+        for(int num : factors) cout << num << ' ';
+        cout << '\n';
+        // value = accumulate(nums_list.begin(),nums_list.end(), 1, multiplies<int>());
+        value = 1;
+        for(int num : nums_list){
+            value *= num;
+        }
+        done = value>500 ? true : false;
         __nums_list = nums_list;
         nums_list.clear();
     }
 
-    cout << value << '\n';
-    cout << a << ' ' << _term << '\n';
-
-    for(int num : factors){
-        cout << num << ' ';
-    }
-    cout << '\n';
+    cout << value << ' ' << _term << '\n';
 }
