482338315a0da69e6d9b710a9b41118c7f6b748d
PeaceTem <olumidejonathan10@gmail.com>
2025-03-09 02:55:52 +0100

    solved the last 1300 to promote to 1400

--- 988/d.cpp
+++ 988/d.cpp
@@ -2,8 +2,53 @@
 using namespace std;
 
 void solve(){
-    int n; cin>>n;
-    cout << n - 1 << '\n';
+    int n,m,L; cin>>n>>m>>L;
+    vector<pair<int, int>> h(n);
+    for(int i=0;i<n;i++){
+        int x,y; cin>>x>>y;
+        h[i] = {x,y};
+    }
+
+
+    vector<pair<int, int>> p(m);
+    for(int i=0;i<m;i++){
+        int x,y; cin>>x>>y;
+        p[i] = {x,y};
+    }
+    
+    sort(p.begin(), p.end(), [&](const pair<int, int>& a, const pair<int, int>& b){
+        return a.second > b.second;
+    });
+
+
+
+    int j = 1, c=0;
+    vector<bool> used(m, false);
+    for(auto& pp : h){
+        if(pp.first == 1 || pp.second == L){
+            cout << -1 << '\n'; return;
+        }
+        int d = pp.second - pp.first + 1;
+        if(d >= j){
+            //find a new power up
+            // int nd = d - j + 1;
+            int l=0, r=p.size() - 1;
+            for(int k=0;k<m;k++){
+                if(used[k]) continue;
+                if(p[k].first > pp.first) continue;
+                j += p[k].second; c++;
+                used[k] = true;
+                if(j>d) break;
+            }
+            if(j<=d){
+                cout << -1 << '\n'; return;
+            }
+        }
+
+
+    }
+
+    cout << c << '\n';
 }
 
 int main(){
