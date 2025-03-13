90148f19a11a48c35982929dd0b0e9529d392bbd
PeaceTem <olumidejonathan10@gmail.com>
2025-03-13 16:18:22 +0100

    prep

--- E175/d.cpp
+++ E175/d.cpp
@@ -1,12 +1,33 @@
 #include <bits/stdc++.h>
 using namespace std;
-typedef long long ll;
+#define ll long long
+#define ull unsigned long long
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+#define endl '\n'
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) (int)(x).size()
+#define lb lower_bound
+#define vi vector<int>
+#define pii pair<int, int>
+#define vii vector<pii>
+#define MOD 1000000007
+#define INF 1000000000000000000
+#define MAXN 1000005
+#define ld long double
+#define eps 1e-9
+#define bit(n, b) (((n) >> (b)) & 1)
+#define bitcount(n) __builtin_popcountll(n)
+#define gcd(a, b) __gcd(a, b)
+// #define fileIO freopen("input.txt", "r", stdin); freopen("output.txt", "w
 
 void solve(){
-    int n; cin>>n;
-    vector<int> a(n);
-    for(int i=0;i<n;i++) cin>>a[i];
+    
 }
+
 int main(){
 	std::ios::sync_with_stdio(false);
 	std::cin.tie(nullptr), std::cout.tie(nullptr);
