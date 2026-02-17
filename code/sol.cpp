657e0b3fed8a5bb269652fa8d2ce23d3b8195609
PeaceTem <olumidejonathan10@gmail.com>
2026-02-18 00:14:30 +0100

    learnt combinatorics on bits

--- code/sol.cpp
+++ code/sol.cpp
@@ -19,49 +19,42 @@ using namespace std;
 #define MAXN 1000005
 #define ld long double
 #define eps 1e-9
+#define int long long
 
-
-void solve(){
-    long long n, ax, ay, bx, by; cin >> n >> ax >> ay >> bx >> by;
-
-    vector<long long> x(n), y(n), dp[2];
-    
-    dp[0] = dp[1] = vector<long long>(n + 6, 0);
-
-    for(long long i = 0; i < n; i++) cin >> x[i];
-    for(long long i = 0; i < n; i++) cin >> y[i];
-    map<long long, long long> mx, mn;
-
-    mx[ax] = mn[ax] = ay;
-    mn[bx] = mx[bx] = by;
-
-    for(long long i = 0; i < n; i++){
-        mx[x[i]] = max(mx[x[i]], y[i]);
-        mn[x[i]] = min(mn.count(x[i]) ? mn[x[i]] : LLONG_MAX, y[i]);
+int cnk[31][31];
+void compute(){
+    for(int i = 0; i < 30; ++i){
+        for(int j = 0; j < 30; ++j){
+            if(i < j) cnk[i][j] = 0;
+            else if(j == 0) cnk[i][j] = 1;
+            else cnk[i][j] = cnk[i - 1][j] + cnk[i - 1][j - 1];
+        }
     }
-    long long cnt = 0, last = ax, need;
-
-    dp[0][0] = dp[1][0] = 0;
-
-    for(auto px : mx){
-        cnt++;
-        need = px.F - last + px.S - mn[px.F];
-
-        dp[0][cnt] = min(dp[0][cnt - 1] + abs(mx[last] - mn[px.F]), dp[1][cnt - 1] + abs(mn[last] - mn[px.F])) + need;
-        dp[1][cnt] = min(dp[0][cnt - 1] + abs(mx[last] - px.S), dp[1][cnt - 1] + abs(mn[last] - px.S)) + need;
-        last = px.F;
-    }
-
-    cout << dp[0][cnt] << endl;
 }
 
-int main() {
+void solve(){
+    int n, k; cin >> n >> k;
+    int d = (int) log2(n);
+    int ans = 0;
+
+    for(int i = 1; i < d; i++){
+        for(int j = 1; j <= i + 1; j++){
+            if(i + j <= k) continue;
+            ans += cnk[i][j - 1];
+        }
+    }
+
+    if(d + 1LL > k) ++ans;
+    cout << ans << endl;
+}
+
+int32_t main() {
     /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
     std::ios::sync_with_stdio(false);
     std::cin.tie(nullptr); std::cout.tie(nullptr);
+    compute();
     int t = 1; 
     cin >> t;
-
     while(t-->0){
         solve();
     }
