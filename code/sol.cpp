e2f7f424adf3f117a2704013b727d6359816fe86
PeaceTem <olumidejonathan10@gmail.com>
2026-02-18 13:54:29 +0100

    finally solved the monopati problem

--- code/sol.cpp
+++ code/sol.cpp
@@ -21,38 +21,52 @@ using namespace std;
 #define eps 1e-9
 #define int long long
 
-int cnk[31][31];
-void compute(){
-    for(int i = 0; i < 30; ++i){
-        for(int j = 0; j < 30; ++j){
-            if(i < j) cnk[i][j] = 0;
-            else if(j == 0) cnk[i][j] = 1;
-            else cnk[i][j] = cnk[i - 1][j] + cnk[i - 1][j - 1];
-        }
-    }
-}
 
 void solve(){
-    int n, k; cin >> n >> k;
-    int d = (int) log2(n);
-    int ans = 0;
+    int n; cin >> n;
+    vector<int> a(n), b(n);
+    for(int i = 0; i < n; i++) cin >> a[i];
+    for(int i = 0; i < n; i++) cin >> b[i];
 
-    for(int i = 1; i < d; i++){
-        for(int j = 1; j <= i + 1; j++){
-            if(i + j <= k) continue;
-            ans += cnk[i][j - 1];
-        }
+    vector<vector<int>> k(4, vector<int>(n));
+    k[0][0] = k[1][0] = a[0];
+    for(int i = 1; i < n; i++){
+        k[0][i] = min(a[i], k[0][i - 1]);
+        k[1][i] = max(a[i], k[1][i - 1]);
+    }
+    k[2].back() = k[3].back() = b.back();
+    for(int i = n - 2; i >= 0; --i){
+        k[2][i] = min(b[i], k[2][i + 1]);
+        k[3][i] = max(b[i], k[3][i + 1]);
     }
 
-    if(d + 1LL > k) ++ans;
+    int ans = 0;
+    vector<pair<int, int>> vp;
+    for(int i = 0; i < n; i++){
+        int ab = min(k[0][i], k[2][i]);
+        int bc = max(k[1][i], k[3][i]);
+        // do prev and next of each.
+
+        // ans += ab * (2 * n - bc + 1);
+        vp.push_back({bc, ab});
+    }
+
+    sort(vp.begin(), vp.end());
+    // int l = 1;
+    int i = 0;
+    for(int l = 1; l <= 2 * n; l++){
+        while(i < n && vp[i].second < l) i++;
+
+        if(i >= n) break;
+        ans += 2 * n - vp[i].first + 1;
+    }
+    
     cout << ans << endl;
 }
 
 int32_t main() {
-    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
     std::ios::sync_with_stdio(false);
     std::cin.tie(nullptr); std::cout.tie(nullptr);
-    compute();
     int t = 1; 
     cin >> t;
     while(t-->0){
