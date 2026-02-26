0f4b20019fd6102df658b3be8476b5477b60bf87
PeaceTem <olumidejonathan10@gmail.com>
2026-02-26 23:43:03 +0100

    learned new bitmask trick backtracking

--- code/sol.cpp
+++ code/sol.cpp
@@ -17,96 +17,25 @@ using namespace std;
 
 
 void solve(){
-    int n, k; cin >> n >> k;
-    vector<int> a(n);
-    for(int i = 0; i < n; i++) cin >> a[i];
-    int cnt = 0;
-    int less_cnt = 0;
-    int g_cnt = 0;
-    int signal = 0;
-    int last = -1;
-    for(int i = 0; i < n; i++){
-        if(a[i] <= k){
-            less_cnt++;
-            // if(signal) signal = 0;
+    int k, x; cin >> k >> x;
+
+    vector<int> ans;
+    int y = (1LL << (k + 1)) - x;
+    while(x != y){
+        if(x < y){
+            ans.push_back(1);
+            y -= x;
+            x <<= 1;
         } else {
-            // if(signal == 0) g_cnt++;
-            // else signal = 0;
-            g_cnt++;
-        }
-
-        if(less_cnt != 0 && less_cnt + 1 == g_cnt && signal){
-            cnt++;
-            // signal = 0;
-            less_cnt = 0;
-            g_cnt = 0;
-            last = i;
-        } else if(less_cnt != 0 && less_cnt == g_cnt){
-            cnt++;
-            less_cnt = 0;
-            g_cnt = 0;
-            last = i;
-        } else if(less_cnt > g_cnt){
-            cnt++;
-            less_cnt = 0;
-            g_cnt = 0;
-            if(i + 1 < n && a[i + 1] > k) signal++;
-            last = i;
+            ans.push_back(2);
+            x -= y;
+            y <<= 1;
         }
     }
-
-    less_cnt = 0;
-    g_cnt = 0;
-    signal = 0;
-
-    int new_cnt = 0;
-    int new_last = -1;
-    for(int i = n - 1; i >= 0; i--){
-        if(a[i] <= k){
-            less_cnt++;
-            // if(signal) signal = 0;
-        } else {
-            // if(signal == 0) g_cnt++;
-            // else signal = 0;
-            g_cnt++;
-        }
-
-        if(less_cnt != 0 && less_cnt + 1 == g_cnt && signal){
-            new_cnt++;
-            signal = 0;
-            less_cnt = 0;
-            g_cnt = 0;
-            new_last = i;
-            // cout << "signal " << a[i] << endl;
-        } else if(less_cnt != 0 && less_cnt == g_cnt){
-            new_cnt++;
-            less_cnt = 0;
-            g_cnt = 0;
-            // signal = 0;
-            new_last = i;
-            // cout << "Equal " << a[i] << endl;
-        } else if(less_cnt > g_cnt){
-            new_cnt++;
-            less_cnt = 0;
-            g_cnt = 0;
-            if(i - 1 >= 0 && a[i - 1] > k) signal++;
-            new_last = i;
-            // cout << "Greater " << a[i] << endl;
-        }
-    }
-    // cout << "What is going on?\n";
-    // cout << "CNT: " << cnt << " NewCNT: " << new_cnt << " Last: " << last << " NewLast: " << new_last << endl;
-
-    if(cnt >= 2 || new_cnt >= 2){
-        cout << "YES\n"; return;
-    }
-
-    if(cnt == 1 && new_cnt == 1 && last != -1 && new_last != -1 && last + 1 < new_last){
-
-        cout << "YES\n"; return;
-    }
-    
-    cout << "NO\n";
+    int p = ans.size();
+    cout << p << endl;
+    for(int i = p - 1; i >= 0; i--) cout << ans[i] << " ";
+    cout << endl;
 }
 
 int32_t main() {
