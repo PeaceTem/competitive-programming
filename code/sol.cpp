68facac5026c7c6e6d30d672c5050982d0bf093b
PeaceTem <olumidejonathan10@gmail.com>
2026-02-28 23:24:24 +0100

    wasted 3hrs on a Div 3F

--- code/sol.cpp
+++ code/sol.cpp
@@ -15,73 +15,99 @@ using namespace std;
 #define eps 1e-9
 #define int long long
 
-vector<int> primes;
-unordered_set<int> st_primes;
-void compute(){
-    vector<bool> is_prime(1e6+1, true);
-    is_prime[0] = is_prime[1] = false;
-    for(int i = 2; i * i <= 1e6; i++){
-        if(is_prime[i]){
-            for(int j = i * i; j <= 1e6; j += i) is_prime[j] = false;
-        }
-    }
-
-    for(int i = 2; i <= 1e6; i++){
-        if(is_prime[i]) primes.push_back(i);
-    }
-    st_primes = unordered_set<int>(all(primes));
-}
-
-vector<int> cmp(int k){
-    int n = primes.size();
-    int i = 0;
-    unordered_set<int> st;
-    while(i < n && primes[i] <= k){
-        if(st_primes.count(k)){
-            st.insert(k); break;
-        }
-        if(k % primes[i] == 0){
-            k /= primes[i];
-            st.insert(primes[i]);
-        } else i++;
-    }
-    return vector<int>(all(st));
-}
+// unordered_map<int, vector<int>> mp;
+vector<vector<int>> vp(2e5 + 5);
+vector<pair<int, int>> pre_max(2e5 + 5);
 
 void solve(){
-    int n; cin >> n;
-    vector<int> a(n);
-    for(int i = 0; i < n; i++) cin >> a[i];
-    bool t = true;
-    for(int i = 1; i < n; i++){
-        if(a[i] < a[i - 1]){
-            t = false; break;
-        }
-    }
-
-    if(t){
-        cout << "Bob\n"; return;
-    }
-
-    int prev_max = 0;
+    int n, m; cin >> n >> m;
+    int x, y;
+    
+    vector<pair<int, int>> a(n);
     for(int i = 0; i < n; i++){
-        if(a[i] == 1){
-            if(a[i] >= prev_max) continue;
-            cout << "Alice\n"; return;
-        }
-
-        vector<int> p = cmp(a[i]);
-        if(p.size() == 0){
-            cout << "Zero\n"; return;
-        }
-        if(p.size() > 1 || p[0] < prev_max){
-            cout << "Alice\n"; return;
-        }
-
-        prev_max = p[0];
+        cin >> x >> y;
+        a[i] = {y, x};
     }
 
-    cout << "Bob\n";
+    sort(all(a));
+    // process from right to left;
+    // store acc;
+    // int inf = 1e18;
+    int cnt = n - 1;
+    priority_queue<pii, vector<pii>, greater<pii>> pq;
+    int acc = 0, last = 0;
+    int tot = 0;
+    for(int i = n; i >= 0; i--){
+        while(cnt >= 0 && a[cnt].first == i){
+            pq.push({a[cnt].second, i});
+            acc += a[cnt].second;
+            while((int) pq.size() > i + 1){
+                acc -= pq.top().first;
+                pq.pop();
+            }
+            cnt--;
+        }
+
+        while((int) pq.size() > i + 1){
+            acc -= pq.top().first;
+            pq.pop();
+        }
+        
+        if((int) pq.size() == 0LL) last = 0;
+        else last = pq.top().first;
+
+        // mp[i] = {acc, last, (int) pq.size()};
+        vp[i] = {acc, last, (int) pq.size()};
+        tot = max(tot, acc);
+    }
+
+    // prefix max;
+    int mx1 = 0, mx2 = -1;
+    // mx1 means nothing to spare.
+    // mx2 means something to spare
+    for(int i = 0; i <= n; i++){
+        if(vp[i][0] >= vp[mx1][0]){
+            mx1 = i;
+        }
+
+        if(vp[i][2] <= i){
+            if(mx2 == -1) mx2 = i;
+            else if(vp[i][0] > vp[mx2][0]) mx2 = i;
+            // else if(vp[i][1] < vp[mx2][1]) mx2 = i;
+        }   
+
+        pre_max[i] = {mx1, mx2};
+    }
+
+    for(int i = 0; i < m; i++){
+        cin >> x >> y;
+
+        if(y > n){
+            cout << max(x, tot) << " ";
+            continue;
+        }
+
+        int acc = vp[y][0];
+        if(vp[y][2] <= y) acc += x;
+        else if (vp[y][2] > y && vp[y][1] < x ){
+            acc += x - vp[y][1];
+        }
+
+        int acc1 = vp[pre_max[y].first][0];
+        if (vp[pre_max[y].first][1] < x ){
+            acc1 += x - vp[pre_max[y].first][1];
+        }
+        
+        int acc2 = -1;
+        if(pre_max[y].second > -1){
+            acc2 = vp[pre_max[y].second][0] + x;
+            // cout << "Got it: " << pre_max[y].second << " " << acc2 << " " << vp[pre_max[y].second][1] << endl;
+        }
+
+        cout << max({tot, acc, acc1, acc2}) << " ";
+    }
+
+    cout << endl;
 }
 
 int32_t main() {
@@ -89,7 +115,6 @@ int32_t main() {
     std::cin.tie(nullptr); std::cout.tie(nullptr);
     int t = 1; 
     cin >> t;
-    compute();
     while(t-->0){
         solve();
     }
