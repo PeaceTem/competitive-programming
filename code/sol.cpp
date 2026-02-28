ab7adc6e02ae4ff3d021936a14d422f3b3a674b5
PeaceTem <olumidejonathan10@gmail.com>
2026-03-01 00:04:25 +0100

    misread Div 3D and struggle a little bit

--- code/sol.cpp
+++ code/sol.cpp
@@ -15,99 +15,54 @@ using namespace std;
 #define eps 1e-9
 #define int long long
 
-// unordered_map<int, vector<int>> mp;
-vector<vector<int>> vp(2e5 + 5);
-vector<pair<int, int>> pre_max(2e5 + 5);
-
 void solve(){
-    int n, m; cin >> n >> m;
-    int x, y;
-    
-    vector<pair<int, int>> a(n);
+    int n, x, y; cin >> n >> x >> y;
+
+    vector<int> p(n);
+    for(int i = 0; i < n; i++) cin >> p[i];
+    int mn = 1e10;
+    int chosen = -1;
+    for(int i = x; i < y; i++){
+        if(p[i] < mn) mn = p[i], chosen = i;
+    }
+    // cout << chosen << " : " << p[chosen] << endl;
+
+    vector<int> kk;
+    for(int i = 0; i < (y - x); i++){
+        if(chosen == y) chosen = x;
+        kk.push_back(p[chosen]);
+        chosen++;
+    }
+
+    vector<int> k;
     for(int i = 0; i < n; i++){
-        cin >> x >> y;
-        a[i] = {y, x};
+        if(i >= x && i < y) continue;
+        k.push_back(p[i]);
     }
 
-    sort(all(a));
-    // process from right to left;
-    // store acc;
-    // int inf = 1e18;
-    int cnt = n - 1;
-    priority_queue<pii, vector<pii>, greater<pii>> pq;
-    int acc = 0, last = 0;
-    int tot = 0;
-    for(int i = n; i >= 0; i--){
-        while(cnt >= 0 && a[cnt].first == i){
-            pq.push({a[cnt].second, i});
-            acc += a[cnt].second;
-            while((int) pq.size() > i + 1){
-                acc -= pq.top().first;
-                pq.pop();
+    int f = 0, s = 0;
+    int a = k.size(), b = kk.size();
+    for(int i = 0; i < 3e10; i++){
+
+        if(f < a && s < b){
+            if(k[f] < kk[s]){
+                cout << k[f] << " "; 
+                f++;
+                continue;
+            } else if(s < b) {
+                for(int j = 0; j < b; j++) cout << kk[j] << " ";
+                s = b;
             }
-            cnt--;
+        } else if(f < a) cout << k[f] << " ", f++;
+        else if(s < b){
+            for(int j = 0; j < b; j++) cout << kk[j] << " ";
+            s = b;
         }
 
-        while((int) pq.size() > i + 1){
-            acc -= pq.top().first;
-            pq.pop();
-        }
-        
-        if((int) pq.size() == 0LL) last = 0;
-        else last = pq.top().first;
-
-        // mp[i] = {acc, last, (int) pq.size()};
-        vp[i] = {acc, last, (int) pq.size()};
-        tot = max(tot, acc);
+        if(f == a && s == b) break;
     }
-
-    // prefix max;
-    int mx1 = 0, mx2 = -1;
-    // mx1 means nothing to spare.
-    // mx2 means something to spare
-    for(int i = 0; i <= n; i++){
-        if(vp[i][0] >= vp[mx1][0]){
-            mx1 = i;
-        }
-
-        if(vp[i][2] <= i){
-            if(mx2 == -1) mx2 = i;
-            else if(vp[i][0] > vp[mx2][0]) mx2 = i;
-            // else if(vp[i][1] < vp[mx2][1]) mx2 = i;
-        }   
-
-        pre_max[i] = {mx1, mx2};
-    }
-
-    for(int i = 0; i < m; i++){
-        cin >> x >> y;
-
-        if(y > n){
-            cout << max(x, tot) << " ";
-            continue;
-        }
-
-        int acc = vp[y][0];
-        if(vp[y][2] <= y) acc += x;
-        else if (vp[y][2] > y && vp[y][1] < x ){
-            acc += x - vp[y][1];
-        }
-
-        int acc1 = vp[pre_max[y].first][0];
-        if (vp[pre_max[y].first][1] < x ){
-            acc1 += x - vp[pre_max[y].first][1];
-        }
-        
-        int acc2 = -1;
-        if(pre_max[y].second > -1){
-            acc2 = vp[pre_max[y].second][0] + x;
-            // cout << "Got it: " << pre_max[y].second << " " << acc2 << " " << vp[pre_max[y].second][1] << endl;
-        }
-
-        cout << max({tot, acc, acc1, acc2}) << " ";
-    }
-
     cout << endl;
+
 }
 
 int32_t main() {
