ee1c274f5c00f52c0b8ac168fc0a3a6890e01b80
PeaceTem <olumidejonathan10@gmail.com>
2026-03-06 22:00:29 +0100

    solved skibidi table

--- code/sol.cpp
+++ code/sol.cpp
@@ -1,112 +1,85 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define ll long long
-#define ull unsigned long long
-#define F first
-#define S second
 #define endl '\n'
 #define all(x) (x).begin(), (x).end()
-#define rall(x) (x).rbegin(), (x).rend()
-#define sz(x) (int)(x).size()
-#define vi vector<int>
-#define pii pair<int, int>
-#define vii vector<pii>
-#define ld long double
-#define eps 1e-9
 #define int long long
 
-
-vector<vector<int>> adj;
-vector<int> cnt;
-vector<int> mx;
-vector<int> ans;
-vector<int> ans_s;
-vector<int> cnt_s;
-
-
-void dfs(vector<int>& a, int p, int parent){
-    int sm = 0, cnt_sm = 0;
-    int mx1 = 0, mx2 = 0;
-    
-    for(int u : adj[p]){
-        if(u == parent) continue;
-
-        dfs(a, u, p);
-
-        if(mx[u] >= mx1){
-            mx2 = mx1;
-            mx1 = mx[u];
-        } else if(mx[u] >= mx2){
-            mx2 = mx[u];
-        }
-
-        sm += cnt[u];
-        // ans_sm += ans_s[u];
-        cnt_sm += cnt_s[u];
-        // cout << sm << " ";
-    }
-
-
-    cnt[p] = a[p] + 2 * sm; // aug_sm
-    cnt_s[p] = a[p] + cnt_sm; // sum
-    mx[p] = mx1 + 1;
-    ans[p] = 0;
-    for(int u : adj[p]){
-        if(u == parent) continue;
-        if(mx[u] == mx1){
-            if(mx2 == 0){
-                ans[p] = max(ans[p], ans[u] + cnt_s[u]);
-            } else {
-                ans[p] = max(ans[p], mx2 * cnt_s[u] + sm);
-            }
-        } else {
-            // ans[p] = max(ans[p], (mx1 - 1) * cnt[u] + sm + cnt_s[u]);
-            ans[p] = max(ans[p], mx1 * cnt_s[u] + sm);
-
-        }
-    }
-
-    
-
-}
-
 void solve(){
-    int n; cin >> n;
+    int n, q; cin >> n >> q;
 
-    vector<int> a(n + 1);
-    for(int i = 1; i <= n; i++) cin >> a[i];
+    // int row = 1LL << n;
+    while(q--){
+        string s; cin >> s;
+        int k = 1LL << (n * 2);
+        int row = 1LL << n;
+        int col = 1LL << n;
+        int d = 0;
 
-    int nn = n;
-    int u, v; 
+        if(s == "->"){
+            int r, c; cin >> r >> c;
+            while(true){
+                if(k == 1){
+                  cout << d + 1 << endl;
+                  break;  
+                }
+                int mask = 0;
 
-    adj.assign(n + 1, vector<int>());
-    cnt.assign(n + 1, 0);
-    mx.assign(n + 1, 0);
-    ans.assign(n + 1, 0);
-    ans_s.assign(n + 1, 0);
-    cnt_s.assign(n + 1, 0);
+                if(r > row / 2) mask |= 1LL << 1;
+                if(c > col / 2) mask |= 1LL;
 
+                if(mask == 0){
+                    
+                } else if(mask == 1){
+                    d +=  3 * (k / 4);
+                    c -= col / 2;
+                } else if(mask == 2){
+                    d += 2 * (k / 4);
+                    r -= row / 2;
+                } else if(mask == 3){
+                    d += k / 4;
+                    r -= row / 2;
+                    c -= col / 2;
+                }
 
+                row >>= 1;
+                col >>= 1;
+                k >>= 2;
+            }
 
-    while(--nn){
-        cin >> u >> v;
-        adj[u].push_back(v);
-        adj[v].push_back(u);
+        } else {
+            int pos; cin >> pos;
+
+            int r = 1, c = 1;
+            while(true){
+                if(k == 1){
+                    cout << r << " " << c << "\n";
+                    break;
+                }
+                if(pos > 3 * (k / 4)){
+                    c += col / 2;
+                    pos -= 3 * (k / 4);
+                } else if(pos > k / 2){
+                    r += row / 2;
+                    pos -=  k / 2;
+                } else if(pos > k / 4){
+                    r += row / 2;
+                    c += row / 2;
+                    pos -= k / 4;
+                }
+
+                col >>= 1;
+                row >>= 1;
+                k >>= 2;
+            }
+        }
     }
 
-    dfs(a, 1, 0);
-
-    for(int i = 1; i <= n; i++){
-        cout << ans[i] << " ";
-    }
-
-    cout << endl;
 }
 
 
 int32_t main() {
     std::ios::sync_with_stdio(false);
-    std::cin.tie(nullptr); std::cout.tie(nullptr);
+    std::cin.tie(nullptr);
     int t = 1; 
     cin >> t;
     while(t-->0){
--- code/sol2.cpp
+++ code/sol2.cpp
@@ -1,79 +1,87 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define ll long long
-#define ull unsigned long long
-#define F first
-#define S second
+
 #define endl '\n'
-#define all(x) (x).begin(), (x).end()
-#define rall(x) (x).rbegin(), (x).rend()
-#define sz(x) (int)(x).size()
-#define vi vector<int>
-#define pii pair<int, int>
-#define vii vector<pii>
-#define ld long double
-#define eps 1e-9
+
 #define int long long
 
+vector<vector<int>> adj;
+vector<int> ans;
+vector<int> sz;
+int n, k;
+
+void dfs(int u, int parent){
+    sz[u] = 1;
+
+    for(int v : adj[u]){
+        if(v == parent) continue;
+
+        dfs(v, u);
+        sz[u] += sz[v];
+    }
+}
+
+void dfs2(int u, int parent){
+    for(int v : adj[u]){
+        if(v == parent) continue;
+
+        int sv = sz[v];
+        int su = sz[u];
+
+        int cur = ans[u];
+
+        if(sv >= k) cur--;
+
+        sz[u] -= sz[v];
+        sz[v] = n;
+
+        if(sz[u] >= k) cur++;
+
+        ans[v] = cur;
+
+        dfs2(v, u);
+
+        sz[u] = su;
+        sz[v] = sv;
+    }
+}
 
 void solve(){
-    int n, h, k; cin >> n >> h >> k;
-    vector<int> a(n);
-    for(int i = 0; i < n; i++) cin >> a[i];
-    int acc = accumulate(all(a), 0LL);
-    int ans = 0;
-    ans += (h / acc) * n + (h / acc)  * k;
-    // int pp = (h / 
-    h %= acc;
-    if(h == 0LL){
-        cout << ans - k << endl; return;
+    cin >> n >> k;
+
+    int nn = n;
+
+    adj.assign(n + 1, vector<int>());
+    ans.assign(n + 1, 0);
+    sz.assign(n + 1, 0);
+
+    while(--nn){
+        int u, v; cin >> u >> v;
+        adj[u].push_back(v);
+        adj[v].push_back(u);
     }
 
-    int hh = h;
-    int mn = LLONG_MAX;
-    // int mn_idx = 1;
-    int mx = LLONG_MIN;
-    for(int i = 0; i < n; i++){
-        if(hh <= a[i]){
-            // find the max element from a[i] -> a[n - 1]
-            mx = *max_element(a.begin() + i, a.end());
-            break;
-        } else hh -= a[i];
+    dfs(1, 0);
+
+    int b = 0;
+
+    for(int i = 0; i <= n; i++){
+        if(sz[i] >= k) b++;
     }
 
-    if(mx >= h){
-        cout << ans + 1 << endl;
-        return;
-    }
+    ans[1] = b;
 
-    vector<int> pref(n + 1);
-    pref[0] = 0;
+    dfs2(1, 0);
 
-    for(int i = 1; i <= n; i++){
-        pref[i] = pref[i - 1] + a[i - 1];
-    }
+    int final_ans = 0;
+    for(int i = 1; i <= n; i++) final_ans += ans[i];
 
-    int need = h - mx;
-    int xx = LLONG_MAX;
-    for(int i = 1; i <= n; i++){
-        int k = need - pref[i - 1];
-        if(k <= 0){
-            xx = min(xx, i);
-            break;
-        }
-        // lower_bound
-        int ind = lower_bound(all(pref), pref[i] + k) - pref.begin();
-
-        xx = min(xx, ind);
-    }
-
-    cout << ans + xx << endl;
-
+    cout << final_ans << endl;
 }
 
 int32_t main() {
     std::ios::sync_with_stdio(false);
-    std::cin.tie(nullptr); std::cout.tie(nullptr);
+    std::cin.tie(nullptr);
     int t = 1; 
     cin >> t;
     while(t-->0){
