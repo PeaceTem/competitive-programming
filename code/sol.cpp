db5d6a8e97f9acd74149aa9ed168257e6bc77057
PeaceTem <olumidejonathan10@gmail.com>
2026-04-30 21:02:56 +0100

    no in-built function reverse function

--- code/sol.cpp
+++ code/sol.cpp
@@ -5,48 +5,18 @@ using namespace std;
 #define int long long
 
 void solve(){
-    int n, x; cin >> n >> x;
+    string s; cin >> s;
 
-    // if(x == n && x % 2 == 0){
-    //     cout << 0 << endl; return;
-    // }
+    int l = 0, r = (int) s.size() - 1;
 
-    int start = max(x - 3, 0LL);
-    int end = min(n, x + 3);
-    // it starts from all even numbers to num +
-    vector<int> dp;
-    for(int i = start; i + 3 <= end; i++){
-        int k = (i) ^ (i + 1) ^ (i + 2) ^ (i + 3);
-        
-        if(k == 0){
-            dp.push_back(i);
-        }
+    while(l < r){
+        char t = s[l];
+        s[l] = s[r];
+        s[r] = t;
+        l++, r--;
     }
 
-    // if((int) dp.size() > 2){
-    //     cout << "Something is wrong" << endl; return;
-    // }
-
-    // if((int) dp.size() == 0){
-    //     cout << 0 << endl;
-
-    // }
-
-    // cout << dp.size() << " " << dp[0] << endl;
-
-    int ans = 0;
-    int mod = 998244353;
-    for(int i = 0; i < (int) dp.size(); i++){
-
-        int left = (dp[i] / 4) % mod;
-        int right = max(0LL, ((n - dp[i] - 3) / 4) % mod);
-
-        int p = ((left + 1) * (right + 1)) % mod;
-        ans = (ans + p) % mod;
-        // cout << dp[i] << " left: " << left << " right: " << right << ' ' << p << endl;
-    }
-
-    cout << ans << endl;
+    cout << s << endl;
 }
  
 int32_t main() {
